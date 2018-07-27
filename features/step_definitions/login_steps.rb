bdfc10bba110fec0de3b669e68dcf549fb4bd80f
Jesse Hooker <jayhook87@gmail.com>
2018-07-27 11:46:14 -0400

    finally got rubymine to see step file

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,13 @@
+
+Given(/^I am on the the Dovico login page$/) do
+  pending
+end
+
+When(/^I enter the username "([^"]*)" with the password "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+Then(/^I am taken to the Dovico homepage$/) do
+  pending
+end
+
