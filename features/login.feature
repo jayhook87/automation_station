049dc6cdf0f140504d2ebaf05f370d1d66b2d18a
Christopher Vaughn <christopher.vaughn@teamnorthwoods.com>
2018-08-24 10:11:29 -0400

    Initializing browser in Cucumber namespace to be picked up by page objects

--- features/login.feature
+++ features/login.feature
@@ -1,6 +1,6 @@
 Feature: Let's log into Dovico
 
   Scenario: I can log into the app as whoever I want
-#    Given I am on the the Dovico login page
+   # Given I am on the the Dovico login page
     When I enter my credentials
     Then I am taken to the Dovico homepage
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,6 @@
 
 When(/^I enter my credentials$/) do
-  browser = Watir::Browser.new :chrome
-  page = LoginPage.new(browser)
-  on(page).login_with('Northwoods','jhooker@teamnorthwoods.com','Lex01lab')
+  on(LoginPage).login_with('Northwoods','jhooker@teamnorthwoods.com','Lex01lab')
 end
 
 Given(/^I am on the the Dovico login page$/) do
